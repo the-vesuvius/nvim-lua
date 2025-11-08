3980bc1ae915df0b9f0cb38da995dfd9d06c0438
the-vesuvius <steven.stas92@gmail.com>
2025-11-08 10:36:39 +0200

    fix: migrate diagnostics signs to new nvim api

--- lua/stefan/plugins/lsp/lspconfig.lua
+++ lua/stefan/plugins/lsp/lspconfig.lua
@@ -71,11 +71,17 @@ return {
     local capabilities = cmp_nvim_lsp.default_capabilities()
 
     -- Change the Diagnostic symbols in the sign column (gutter)
-    -- (not in youtube nvim video)
-    local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
-    for type, icon in pairs(signs) do
-      local hl = "DiagnosticSign" .. type
-      vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-    end
+    local severity = vim.diagnostic.severity
+
+    vim.diagnostic.config({
+      signs = {
+        text = {
+          [severity.ERROR] = " ",
+          [severity.WARN] = " ",
+          [severity.HINT] = "󰠠 ",
+          [severity.INFO] = " ",
+        },
+      },
+    })
   end,
 }
